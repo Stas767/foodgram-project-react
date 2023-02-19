8dff89a0f92b58ee07b1eb1cdc50dfb9a06a13de
Стас Балджи <stack767@gmail.com>
2023-02-19 23:56:20 +0300

    Изменения:
    - сделал новую структуру проекта, поместил модели в одном приложении;
    - удалил лишние приложения;
    - зарегистрировал 3 модели в админке.

--- backend/foodgram/recipes/admin.py
+++ backend/foodgram/recipes/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Tag, Ingredient, Recipe
+
+
+class RecipeIngredientsInlineAdmin(admin.TabularInline):
+    model = Recipe.ingredients.through
+    min_num = 1
+    extra = 0
+
+    def get_formset(self, request, obj=None, **kwargs):
+        formset = super().get_formset(request, obj=None, **kwargs)
+        formset.validate_min = True
+        return formset
+
+
+class RecipeIngredientsAdmin(admin.ModelAdmin):
+    inlines = [RecipeIngredientsInlineAdmin]
+
+
+class RecipeTagsInlineAdmin(admin.TabularInline):
+    model = Recipe.tags.through
+    min_num = 1
+    extra = 0
+
+    def get_formset(self, request, obj=None, **kwargs):
+        formset = super().get_formset(request, obj=None, **kwargs)
+        formset.validate_min = True
+        return formset
+
+
+class RecipeTagsAdmin(admin.ModelAdmin):
+    inlines = [RecipeTagsInlineAdmin]
+
+
+# admin.site.register(User)
+admin.site.register(Tag)
+admin.site.register(Ingredient)
+admin.site.register(Recipe, RecipeIngredientsAdmin)
--- backend/foodgram/recipes/models.py
+++ backend/foodgram/recipes/models.py
@@ -1,16 +1,51 @@
 from django.db import models
-from users.models import User
-from ingredients.models import Ingredient, IngredientRecipe
-from tags.models import Tag, TagRecipe
+# from users.models import User
+
+
+class Tag(models.Model):
+    name = models.CharField(
+        'Название',
+        max_length=200,
+        unique=True
+    )
+    color = models.CharField(
+        'Цвет в HEX',
+        max_length=7,
+        unique=True
+    )
+    slug = models.SlugField(
+        'Уникальный слаг',
+        unique=True
+    )
+
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+
+
+class Ingredient(models.Model):
+    name = models.CharField(
+        'Название',
+        max_length=200
+    )
+    measurement_unit = models.CharField(
+        'Единицы измерения',
+        max_length=200
+    )
+    amount = models.PositiveIntegerField('Количество')
+
+    class Meta:
+        verbose_name = 'Ингредиент'
+        verbose_name_plural = 'Ингредиенты'
 
 
 class Recipe(models.Model):
-    author = models.ForeignKey(
-        User,
-        on_delete=models.SET_NULL,
-        related_name='recipes',
-        verbose_name='Автор'
-    )
+    # author = models.ForeignKey(
+    #     User,
+    #     on_delete=models.SET_NULL,
+    #     related_name='recipes',
+    #     verbose_name='Автор'
+    # )
     name = models.CharField(
         max_length=200,
         verbose_name='Название'
@@ -22,14 +57,14 @@ class Recipe(models.Model):
     text = models.TextField('Описание рецепта')
     ingredients = models.ManyToManyField(
         Ingredient,
-        through=IngredientRecipe,
-        through_fields=('ingredient', 'recipe'),
+        through='IngredientRecipe',
+        through_fields=('recipe', 'ingredient'),
         verbose_name='Ингредиенты'
     )
     tags = models.ManyToManyField(
         Tag,
-        through=TagRecipe,
-        through_fields=('tag', 'recipe'),
+        through='TagRecipe',
+        through_fields=('recipe', 'tag'),
         verbose_name='Теги'
     )
     cooking_time = models.PositiveIntegerField('Время приготовления в минутах')
@@ -42,3 +77,39 @@ class Recipe(models.Model):
         ordering = [('-pub_date')]
         verbose_name = 'Рецепт'
         verbose_name_plural = 'Рецепты'
+
+
+class IngredientRecipe(models.Model):
+    ingredient = models.ForeignKey(
+        Ingredient,
+        on_delete=models.SET_NULL,
+        null=True,
+        verbose_name='Ингредиент'
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.SET_NULL,
+        null=True,
+        verbose_name='Рецепт'
+    )
+
+    def __str__(self):
+        return f'{self.ingredient} {self.recipe}'
+
+
+class TagRecipe(models.Model):
+    tag = models.ForeignKey(
+        Tag,
+        on_delete=models.SET_NULL,
+        null=True,
+        verbose_name='Тег'
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.SET_NULL,
+        null=True,
+        verbose_name='Рецепт'
+    )
+
+    def __str__(self):
+        return f'{self.tag} {self.recipe}'
