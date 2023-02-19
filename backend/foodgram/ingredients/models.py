53378a9b70899ff9a32c38249bed657ebf8453ac
Стас Балджи <stack767@gmail.com>
2023-02-19 21:53:34 +0300

    Первый вариант моделей БД

--- backend/foodgram/ingredients/models.py
+++ backend/foodgram/ingredients/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from recipes.models import Recipe
 
-# Create your models here.
+
+class Ingredient(models.Model):
+    name = models.CharField(
+        'Название',
+        max_length=200
+    )
+    measurement_unit = models.CharField(
+        'Единицы измерения',
+        max_length=200
+    )
+    amount = models.PositiveIntegerField('Количество')
+
+    class Meta:
+        verbose_name = 'Ингредиент'
+        verbose_name_plural = 'Ингредиенты'
+
+
+class IngredientRecipe(models.Model):
+    ingredient = models.ForeignKey(
+        Ingredient,
+        on_delete=models.SET_NULL,
+        null=True,
+        verbose_name='Ингредиент'
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.SET_NULL,
+        null=True,
+        verbose_name='Рецепт'
+    )
+
+    def __str__(self):
+        return f'{self.ingredient} {self.recipe}'
--- backend/foodgram/recipes/models.py
+++ backend/foodgram/recipes/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from users.models import User
+from ingredients.models import Ingredient, IngredientRecipe
+from tags.models import Tag, TagRecipe
 
-# Create your models here.
+
+class Recipe(models.Model):
+    author = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        related_name='recipes',
+        verbose_name='Автор'
+    )
+    name = models.CharField(
+        max_length=200,
+        verbose_name='Название'
+    )
+    image = models.ImageField(
+        upload_to='recipes/',
+        verbose_name='Картинка рецепта'
+    )
+    text = models.TextField('Описание рецепта')
+    ingredients = models.ManyToManyField(
+        Ingredient,
+        through=IngredientRecipe,
+        through_fields=('ingredient', 'recipe'),
+        verbose_name='Ингредиенты'
+    )
+    tags = models.ManyToManyField(
+        Tag,
+        through=TagRecipe,
+        through_fields=('tag', 'recipe'),
+        verbose_name='Теги'
+    )
+    cooking_time = models.PositiveIntegerField('Время приготовления в минутах')
+    pub_date = models.DateTimeField(
+        'Дата публикации',
+        auto_now_add=True,
+    )
+
+    class Meta:
+        ordering = [('-pub_date')]
+        verbose_name = 'Рецепт'
+        verbose_name_plural = 'Рецепты'
