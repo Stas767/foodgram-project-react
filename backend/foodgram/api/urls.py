fadbc5b2e5e27aea85e09079d537bf4b0f047112
Стас Балджи <stack767@gmail.com>
2023-03-19 13:33:48 +0300

    Добавил роутер для обработки своих actions

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -1,16 +1,21 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from api.views import IngredientViewSet, RecipeViewSet, TagViewSet
-
+from api.views import (
+    IngredientViewSet,
+    RecipeViewSet,
+    TagViewSet,
+    CustomUserViewSet
+)
 
 router = DefaultRouter()
 router.register('tags', TagViewSet)
 router.register('recipes', RecipeViewSet)
 router.register('ingredients', IngredientViewSet)
+router.register('users', CustomUserViewSet)
 
 
 urlpatterns = [
-    path('', include('users.urls')),
     path('', include(router.urls)),
+    path('', include('users.urls')),
 ]
