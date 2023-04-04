c5b1da2acea86b084a058c21772d263a43313d08
Стас Балджи <stack767@gmail.com>
2023-04-04 12:22:16 +0300

    Написал бекенд фильрации для модели рецептов

---
+++ backend/foodgram/api/filters.py
@@ -0,0 +1,35 @@
+import django_filters
+
+from recipes.models import Recipe, Tag
+
+
+class RecipeFilter(django_filters.FilterSet):
+    '''Фильтрация по вложенным полям в модели Recipe.'''
+
+    tags = django_filters.ModelMultipleChoiceFilter(
+        field_name='tags__slug',
+        to_field_name='slug',
+        queryset=Tag.objects.all()
+    )
+    is_favorited = django_filters.NumberFilter(
+        method='filter_is_favorited',
+    )
+    is_in_shopping_cart = django_filters.NumberFilter(
+        method='filter_is_in_shopping_cart',
+    )
+
+    class Meta:
+        model = Recipe
+        fields = ('author', 'tags', 'is_favorited', 'is_in_shopping_cart', )
+
+    def filter_is_favorited(self, queryset, name, value):
+        user = self.request.user
+        if value and not user.is_anonymous:
+            return queryset.filter(favorites__user=self.request.user)
+        return queryset
+
+    def filter_is_in_shopping_cart(self, queryset, name, value):
+        user = self.request.user
+        if value and not user.is_anonymous:
+            return queryset.filter(shopping_carts__user=self.request.user)
+        return queryset
